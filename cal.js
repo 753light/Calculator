15ec76445f4901a59f3dd901e07cdd12d4eb1cb5
Temi Akanbi <oluwatobiakanbi200@gmail>
2023-04-25 20:02:00 -0700

    modified work

--- cal.js
+++ cal.js
@@ -1,20 +1,27 @@
-let equation = '';
-function updateDisplay(value){
-    const display = document.getElementById('display');
-    equation += value;
-    display.value = equation;
+let screen = document.getElementById("display");
+buttons = document.querySelectorAll("button");
+let screenValue = "";
+for (item of buttons) {
+  item.addEventListener("click", (e) => {
+    buttonText = e.target.innerText;
+    if (buttonText == "X") {
+      buttonText = "*";
+      screenValue += buttonText;
+      screen.value = screenValue;
+    } else if (buttonText == "C") {
+      screenValue = "";
+      screen.value = screenValue;
+    } else if (buttonText == "=") {
+      var result;
+      try {
+        result = eval(screenValue);
+      } catch (error) {
+        result = "Expression error";
+      }
+      screen.value = result;
+    } else {
+      screenValue += buttonText;
+      screen.value = screenValue;
+    }
+  });
 }
-
-function clearDisplay(){
-    const display = document.getElementById('display');
-    equation = '';
-    display.value = equation;
-}
-
-function calculate(){
-    const display = document.getElementById('display');
-    let result = eval(equation);
-    display.value = result.toFixed(2);
-    equation = result.toFixed(2);
-}
-jj
